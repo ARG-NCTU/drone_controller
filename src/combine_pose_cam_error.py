54d7d3c25fe353b9729c94a946316b26ee14d71b
sunfu-chou <sunfu.chou@gmail.com>
2024-06-25 13:13:10 +0800

    Update combine_pose_cam_error.py

--- src/combine_pose_cam_error.py
+++ src/combine_pose_cam_error.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 import rospy
+from behavior_tree_msgs.msg import Active, Status
 from geometry_msgs.msg import PoseStamped, Twist
+
+FAIL = 0
+RUNNING = 1
+SUCCESS = 2
 
 
 class CombinePoseCamError:
     def __init__(self):
         self.rate = float(rospy.get_param("~rate", 10.0))
 
-        self.pose_sub = rospy.Subscriber("/pozyx_simulation/drone/pose/optim", PoseStamped, self.pose_callback)
+        self.pose_sub = rospy.Subscriber("/pozyx_simulation/drone/pose/ground_truth", PoseStamped, self.pose_callback)
         self.cam_error_sub = rospy.Subscriber("/drone/perception/target/error", Twist, self.cam_error_callback)
         self.controller_error_pub = rospy.Publisher("/drone/controller/target/error", Twist, queue_size=1)
 
+        self.drone_move_forward_status_pub = rospy.Publisher("/drone_move_forward_status", Status, queue_size=10)
+        rospy.Subscriber("/drone_move_forward_active", Active, self.active_callback)
+
+        self.active = False
+
+        self.target_x = -1.6
         self.pose = None
         self.cam_error = None
         self.controller_error = Twist()
@@ -25,14 +36,30 @@ class CombinePoseCamError:
     def timer_callback(self, event):
         if self.pose is None or self.cam_error is None:
             return
-        self.controller_error.linear.x = -self.pose.pose.position.x - 3.0
+        if self.active:
+            if self.target_x < -0.2:
+                self.target_x += 0.06
+            else:
+                self.target_x = -1.2
+        # else:
+            # self.target_x = -2.0
+        print("target x: {}".format(self.target_x))
+
+        self.controller_error.linear.x = -self.pose.pose.position.x + self.target_x
         # self.controller_error.linear.x = self.cam_error.linear.z
         self.controller_error.linear.y = -self.pose.pose.position.y
         self.controller_error.linear.z = self.cam_error.linear.z
-        self.controller_error.angular.z = self.cam_error.linear.y
+        self.controller_error.angular.z = self.cam_error.linear.y + 0.2
         self.controller_error_pub.publish(self.controller_error)
         self.pose = None
         self.cam_error = None
+
+    def active_callback(self, msg):
+        self.active = msg.active
+        status = Status()
+        status.id = msg.id
+        status.status = SUCCESS
+        self.drone_move_forward_status_pub.publish(status)
 
 
 if __name__ == "__main__":
